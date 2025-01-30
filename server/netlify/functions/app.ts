e7f82e171b299e6e8603db75ad935f210c773806
MazenHassan10 <mazen0000072@gmail.com>
2025-01-30 13:00:00 +0200

    trying with netlify again 2

--- server/netlify/functions/app.ts
+++ server/netlify/functions/app.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { expensesRoute } from "./routes/expenses";
+import { expensesRoute } from "../../routes/expenses";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
 const app = new Hono();
