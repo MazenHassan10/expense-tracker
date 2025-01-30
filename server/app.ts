ee3cc06c46628f3b7b8af010282c061aa815554a
MazenHassan10 <mazen0000072@gmail.com>
2025-01-30 12:31:32 +0200

    trying netlify again

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { expensesRoute } from "./routes/expenses";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
+import { handle } from "hono/netlify";
 const app = new Hono();
 app.use(logger());
 
@@ -10,4 +11,4 @@ app.route("/api/expenses", expensesRoute);
 app.use("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
-export default app;
+export default handle(app);
